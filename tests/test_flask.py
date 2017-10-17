3ba9a5d97217af2475ea30ce0c9ee4e8d76a206e
hfiedler <harald.fiedler@cavorit.de>
2017-10-17 10:31:34 +0200

    renamed tests

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -4,7 +4,7 @@ mount_folders.mount() # sets other projectfolders in searchpath
 
 from app import app # this is the flask test
 
-class HelloWorldTestCase(unittest.TestCase):
+class Flask_Test(unittest.TestCase):
 
 
     def setUp(self):
@@ -14,13 +14,13 @@ class HelloWorldTestCase(unittest.TestCase):
 
     def test_models_redirect(self):
         r = self.app.get('/models/handshake')
-        assert r.status_code = 200
+        assert r.status_code == 200
         assert b'/models folder is now /services' in r.data
 
     def test_services_req(self):
         r = self.app.get('/services/handshake')
-        assert r.status_code = 200
-        assert b'msg' in r.data
+        assert r.status_code == 200
+        assert b"Service is a smile. It is an acknowledging wave, a reaching handshake, a friendly wink, and a warm hug. It's these simple acts that matter most, because the greatest service to a human soul has always been the kindness of recognition. [R. Goodrich]" in r.data
 
 if __name__ == '__main__':
     unittest.main()
