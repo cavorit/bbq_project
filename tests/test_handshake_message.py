e5ddb93e55bde164a0700e9c1ea83c80a572c28c
hfiedler <harald.fiedler@cavorit.de>
2017-10-17 09:04:07 +0200

    cleaning + adjusting unittest to nameconvention of yhat

--- tests/test_handshake_message.py
+++ tests/test_handshake_message.py
@@ -1,10 +1,10 @@
 import unittest
 import mount_folders
 mount_folders.mount() # sets other projectfolders in searchpath
-from handshake_message import handshake_message # must be after mount
+from handshake_message import modelPredict # must be after mount
 
 
-class test_handshake_message(unittest.TestCase):
+class test_modelPredict(unittest.TestCase):
     
     def test_return_value(self):
         msg = "Service is a smile. It is an acknowledging wave, a reaching handshake, a friendly wink, and a warm hug. It's these simple acts that matter most, because the greatest service to a human soul has always been the kindness of recognition. [R. Goodrich]"
