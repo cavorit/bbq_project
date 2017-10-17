409232ea518d6c08f155ad47e0c4f90c5eed84c8
hfiedler <harald.fiedler@cavorit.de>
2017-10-17 09:07:34 +0200

    bugfix in unittest

--- tests/test_handshake_message.py
+++ tests/test_handshake_message.py
@@ -8,8 +8,8 @@ class test_modelPredict(unittest.TestCase):
     
     def test_return_value(self):
         msg = "Service is a smile. It is an acknowledging wave, a reaching handshake, a friendly wink, and a warm hug. It's these simple acts that matter most, because the greatest service to a human soul has always been the kindness of recognition. [R. Goodrich]"
-        self.assertIsNotNone(handshake_message)
-        self.assertEqual(handshake_message(), msg) 
+        self.assertIsNotNone(modelPredict())
+        self.assertEqual(modelPredict(), msg) 
 
 if __name__ == "__main__":
     unittest.main()
